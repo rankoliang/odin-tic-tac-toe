2b65c7ebce7c3814b5250e167613cac749ed0037
Ranko Liang <rankoliang@gmail.com>
2020-06-07 02:01:40 -0400

    Add Player class

--- main.rb
+++ main.rb
@@ -1,17 +1,32 @@
+# frozen_string_literal: true
+
 class Board
   attr_reader :board
 
   def initialize(board_size)
-    self.board = Array.new(board_size,Array.new(board_size, 0))
+    self.board = Array.new(board_size, Array.new(board_size, 0))
   end
 
   def display
-    self.board.each {|row| p row}
+    board.each { |row| p row }
   end
 
   private
 
   attr_writer :board
+end
+
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    self.name = name
+    self.symbol = symbol
+  end
+
+  private
+
+  attr_writer :name, :symbol
 end
 
 Board.new(3).display
